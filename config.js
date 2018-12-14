f32cb597a7c8b1c842d8dbfa476d090a07cda24e
Esteban <esteban93torres@gmail.com>
2018-12-13 23:43:38 -0500

    mailer

---
+++ config.js
@@ -0,0 +1,6 @@
+const mail = {
+  user: 'YOUR GMAIL',
+  pass: 'YOUR PASSWORD'
+}
+// Important allow acces to the app on https://myaccount.google.com/lesssecureapps
+module.exports = mail;
---
+++ mailer.js
@@ -0,0 +1,53 @@
+const nodemailer = require('nodemailer');
+const configMail = require('./config')
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+         user: configMail.user,
+         pass: configMail.pass
+     }
+ });
+
+const participants = [
+  //Fill it with your participants
+  {name: '', mail: ''},
+  {name: '', mail: ''},
+  {name: '', mail: ''}
+]
+const taken = []
+
+for ( let i = 0; i < participants.length; i++) {
+  const participant = participants[i];
+  const random = randomFriend();
+  matchParticipants(participant,random);
+}
+function randomFriend() {
+  const randomNumber = Math.floor(Math.random() * participants.length);
+  const randomParticipant = participants[randomNumber];
+  return randomParticipant;
+}
+function  matchParticipants(participant, randomParticipant) {
+  if (participant !== randomParticipant && !taken.includes(randomParticipant)) {
+    taken.push(randomParticipant);
+    sendMail(participant, randomParticipant);
+    /* console.log(participant , randomParticipant); */
+    return
+  }
+  return  matchParticipants(participant, randomFriend())
+}
+function sendMail(participant, partner) {
+  mailOptions = {
+    from: configMail.user , // sender address
+    to: participant.mail, // receiver
+    subject: 'Sorteo Amigo Secreto', // Subject line
+    html: `<p> Hola ${participant.nombre} tu pareja es ${partner.nombre}</p>`// plain text body
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+  });
+}
+
